44f01165e1fefd082a7d61ba630b0011e45498a5
AEljaa <adamtantalum@gmail.com>
2024-05-30 15:09:14 +0100

    Algo can be imported as a pyhton module and the main can be called

--- algorithm/algo.py
+++ algorithm/algo.py
@@ -1,4 +1,8 @@
-import requests,time
+import requests
+import json
+
+url="http://127.0.0.1:4000/algorithm" #from our flask api
+
 def naive_algorithm(price,yesterday_list):
     if(price < sum(yesterday_list)/len(yesterday_list)):
         print("BUY")
@@ -7,28 +11,31 @@ def naive_algorithm(price,yesterday_list):
 
 def algorithm(price, yesterday_list, demand): #Sophie's aproach - need to refine within quartiles 2 and 3 (currently do nothing)
     yesterday_list.sort()
-    median = yesterday_list[len(yesterday_list) // 2]
-    mean = sum(yesterday_list) / len(yesterday_list)
+    instruction=""
+    ratio=0
     q3_index = int(0.75 * len(yesterday_list))  # Convert float index to integer
     q3 = yesterday_list[q3_index]
     q1_index = len(yesterday_list) // 4
     q1 = yesterday_list[q1_index]
-    print(f"median {median} mean {mean} q3 {q3} q1 {q1}")
-    # Just now need to connec this to a socket and send the response and amount -- TODO
     if price < q1:
-        print(f"BUY at {1+demand/4} multiplier at price {price}")
+        instruction="BUY"
+        ratio=1+demand/4
     elif price >q3:
-        print(f"SELL at {1+demand/4} multiplier at price {price}")
+        instruction="SELL"
+        ratio=1+demand/4
+    value= {
+        "instruction" : instruction,
+        "ratio" : ratio
+    }
 
+    return json.dumps(value)
 
-url="http://127.0.0.1:4000/algorithm" #from our flask api
-while True:
+def main():
     source = requests.get(url).json()
     price=source['current_sell_price']
     yesterday_list=source['yesterday_sell_prices']
     demand=source['demand']
-    algorithm(price,yesterday_list,demand)
-    time.sleep(5)
+    return algorithm(price,yesterday_list,demand)
 
     
 
