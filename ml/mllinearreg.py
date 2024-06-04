268f28bf88d0f5fe71574a6c9e0e939ecea56317
AEljaa <adamtantalum@gmail.com>
2024-06-04 23:06:33 +0100

    Bit more accurate algo

--- ml/mllinearreg.py
+++ ml/mllinearreg.py
@@ -58,11 +58,14 @@ model = joblib.load("RandomForrestModelFut2.pkl")
 while True:
     inputData,price=fetch_latest()
     predictedSellPrices=model.predict(inputData)
-    grad = all(i < j for i, j in zip(predictedSellPrices[0], predictedSellPrices[0][1:]))#https://www.geeksforgeeks.org/python-check-if-list-is-strictly-increasing/  if pred array increases the nprice trending up, buy
-    if grad:
+    incr = all(i < j for i, j in zip(predictedSellPrices[0], predictedSellPrices[0][1:]))#https://www.geeksforgeeks.org/python-check-if-list-is-strictly-increasing/  if pred array increases the price trending up, buy
+    dec_list=np.diff(predictedSellPrices)
+    decr = np.all(dec_list < 0)#https://www.geeksforgeeks.org/python-test-for-strictly-decreasing-list/ if pred array decreases the price trending down, sell
+
+    if incr:
         wallet -= price
         print("BUY: wallet",wallet)
-    else:
+    if decr:
         wallet+=price
         print("SELL: wallet",wallet)
     time.sleep(5)
