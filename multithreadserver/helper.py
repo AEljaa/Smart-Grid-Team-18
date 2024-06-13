cdb9bf7a4b3896cbb76cb1d1e9ec9ed051d6d64b
AEljaa <adamtantalum@gmail.com>
2024-06-13 13:27:53 +0100

    refining

--- multithreadserver/helper.py
+++ multithreadserver/helper.py
@@ -4,11 +4,6 @@ import pandas as pd
 import numpy as np
 import joblib
 url="http://127.0.0.1:4000/helper" #from our flask api
-<<<<<<< HEAD
-model = joblib.load(r"C:\Users\Student\Desktop\summerproj\SummerProjWeb\multithreadserver\ml.pkl")
-#load ml model
-wallet=0
-=======
 #model = joblib.load("ml.pkl")
 model = joblib.load(r"C:\Users\Student\Desktop\summerproj\SummerProjWeb\multithreadserver\ml.pkl")
 wallet=0
@@ -66,66 +61,3 @@ def return_irradiance():
 def return_demand():
     _,_,_,_,demand,_=fetch_latest()
     return demand
->>>>>>> f14093f (refining)
-
-"""
-def fetch_latest():
-    source = requests.get(url).json()
-    lags = source["lags"]
-    price = source['current_sell_price']
-    demand = source['demand']
-    tick = source['tick']
-    irradiance = source ["sun"]
-    inputData = pd.DataFrame(
-        {
-            "demandHist": demand,
-            "tick": tick,
-            "Lag_1": lags[-1],
-            "Lag_2": lags[-2],
-            "Lag_3": lags[-3]
-        }, index=[0])
-    return inputData, price, tick, lags,demand,irradiance
-
-
-def naive_algorithm(price,yesterday_list):
-    if(price < sum(yesterday_list)/len(yesterday_list)):
-        print("BUY")
-    else:
-        print("SELL")
-
-def algorithm(MaxAmount):    
-    input,_,_,_,_,_=fetch_latest()
-    predicted_prices=model.predict(input)
-    test_list=predicted_prices[0]
-    print(test_list)
-    decr=0
-    incr=0
-    ratio=return_demand()/5
-    res=0
-    decr = np.array_equal(test_list, sorted(test_list, reverse=True))
-    incr = all(i < j for i, j in zip(predicted_prices[0], predicted_prices[0][1:]))#https://www.geeksforgeeks.org/python-check-if-list-is-strictly-increasing/  if pred array increases the nprice trending up, buy
-    if(decr):
-        res=MaxAmount*ratio*-1
-    if incr:
-        res=MaxAmount*ratio
-    value= {
-        "instruction" : res,
-        "ratio" : ratio
-    }
-    return value
-
-
-def return_irradiance():
-    _,_,_,_,_,irradiance = fetch_latest()
-    return irradiance
-
-
-def return_demand():
-    _,_,_,_,demand,_=fetch_latest()
-    return demand
-
-
-    
-
-
-"""
